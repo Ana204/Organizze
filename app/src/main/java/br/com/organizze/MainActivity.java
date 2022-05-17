ecdbcd20ee6c93669eeec924ecebb743f9fde4d1
ana204 <anagomes.lucia@hotmail.com>
2022-05-17 17:39:15 -0300

    build: environment configuration
    The environment was configured for build.gradle to use the slide
    
    build.gradle configuration

--- app/src/main/java/br/com/organizze/MainActivity.java
+++ app/src/main/java/br/com/organizze/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
