f07ac60c657d6dda368529d637f00aa79bf4f0fa
ana204 <anagomes.lucia@hotmail.com>
2022-05-24 11:59:33 -0300

    style: New fragments were created
    A new fragment is added to slide
    
    fragment

---
+++ app/src/main/java/br/com/organizze/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.com.organizze.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.organizze.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/br/com/organizze/MainActivity.java
+++ app/src/main/java/br/com/organizze/MainActivity.java
@@ -3,15 +3,66 @@ package br.com.organizze;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class MainActivity extends IntroActivity {
+
+    TextView textViewAccount;
+    Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.activity_main);
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.welcome)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.information)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.alert)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.organizze_intro)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.account_registration)
+                .canGoForward(false)
+                .build()
+        );
+
+    }
+
+
+    public void buttonAccount(View view){
+        Toast.makeText(this, "Já tenho conta CLICK", Toast.LENGTH_SHORT).show();
+    }
+
+    public void buttonRegister(View view){
+
     }
 }
